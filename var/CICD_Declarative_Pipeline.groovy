fd50be93d97c1bd3f3e16e53a8a74a1bc4f3ba94
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-01-18 13:30:58 +0530

    Update CICD_Declarative_Pipeline.groovy

--- var/CICD_Declarative_Pipeline.groovy
+++ var/CICD_Declarative_Pipeline.groovy
@@ -1,14 +1,23 @@
+import com.demo.util.checkoutSCM;
 pipeline {
     agent any
     environment {
-    
+    BRANCH = env.BRANCH
+    REPO = env.REPO
   }
 
     stages {
         stage('checkout SCM') {
             steps { 
                 echo 'Step to checkout the code from github'
-                
+                env.SCM_URL="git@github.com:"+pipelineParams.GIT_GROUP+"/"+pipelineParams.REPO+".git"
+                echo "Code checkout from SCM Repo"
+                sh ''' 
+                rm -rf ${REPO}
+                git clone --single-branch --branch ${BRANCH} ${SCM_URL}
+                ''' 
+                echo "Checkout is completed!"
+                }
             }
         }
     }
