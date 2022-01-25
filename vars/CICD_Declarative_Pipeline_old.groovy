17a5eefc88916b01c35be865a0fa3b129c1977b6
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-01-25 15:36:33 +0530

    Create CICD_Declarative_Pipeline_old.groovy

---
+++ vars/CICD_Declarative_Pipeline_old.groovy
@@ -0,0 +1,61 @@
+import com.poc.util.checkoutSCM;
+
+def call(Map pipelineParams)
+{
+pipeline {
+    
+    agent any
+    
+    /* environment 
+            {
+                BRANCH = 'master'
+                REPO = 'hello-world-1'
+                GIT_GROUP='minaxijoshi3101'
+                SCM_URL=''
+            } */
+   /* parameters {
+        choice {
+                name: 'BuildType',
+                choices: 'SNAPSHOT\nRELEASE',
+                description: 'choose the build type'
+        }
+    }*/
+    tools{
+        jdk 'openjdk-1.11.0'
+        maven 'maven'
+    }
+    stages {
+        stage('checkout SCM') {
+            steps { 
+                script{
+                echo 'Step to checkout the code from github'
+                sh """
+                cd $WORKSPACE
+                """
+                new checkoutSCM().call(pipelineParams)
+                }
+                 }
+            }
+        stage ('build code') {
+            steps {
+                echo "build a java code using mvn"
+                sh """
+                cd ${REPO}
+                mvn clean install package
+                """
+            }
+            
+        }
+        stage ("deploy")
+        {
+            steps
+            {
+                echo "deploy war to tomcat app server"
+                sh """
+                  scp -i /etc/key.pem -r /root/.jenkins/workspace/pipeline_pocs/first_pippeline/webapp/target/*.war ec2-user@65.0.4.77:/app/apache-tomcat-9.0.56/webapps
+                """
+            }
+        }
+      }
+}
+}
