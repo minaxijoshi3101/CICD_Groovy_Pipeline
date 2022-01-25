543812a41b5ab1e8bcd4e2a0aa46750366de23cd
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-01-25 23:52:25 +0530

    Update CICD_Declarative_Pipeline.groovy

--- vars/CICD_Declarative_Pipeline.groovy
+++ vars/CICD_Declarative_Pipeline.groovy
@@ -13,25 +13,22 @@ pipeline {
             """
             new checkoutSCM().call(pipelineParams)
         }       
-       stage ('build code') {
-            steps {
+       stage ('build code') 
+        {    
                 echo "build a java code using mvn"
                 sh """
                 cd ${REPO}
                 mvn clean install package
-                """
-            }
-            
+                """    
         }
         stage ("deploy")
         {
-            steps
-            {
+            
                 echo "deploy war to tomcat app server"
                 sh """
                   scp -i /etc/key.pem -r /root/.jenkins/workspace/pipeline_pocs/first_pippeline/webapp/target/*.war ec2-user@65.0.4.77:/app/apache-tomcat-9.0.56/webapps
                 """
-            }
+            
         }
         
       }
