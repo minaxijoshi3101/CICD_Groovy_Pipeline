9033f5d0d8fc9dbb2c780c180b61b5941c34eefb
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-01-26 15:46:10 +0530

    Update CICD_Declarative_Pipeline.groovy

--- vars/CICD_Declarative_Pipeline.groovy
+++ vars/CICD_Declarative_Pipeline.groovy
@@ -3,34 +3,34 @@ def call(Map pipelineParams)
 {
     env.BRANCH = pipelineParams.BRANCH
     env.REPO = pipelineParams.REPO
-pipeline {
-    node(){
-        stage("checkout SCM") 
-        {
-            echo 'Step to checkout the code from github'
-            sh """
-            cd $WORKSPACE
-            """
-            new checkoutSCM().call(pipelineParams)
-        }       
-       stage ('build code') 
-        {    
-                echo "build a java code using mvn"
+    pipeline {
+        node(){
+            stage("checkout SCM") 
+            {
+                echo 'Step to checkout the code from github'
                 sh """
-                cd ${REPO}
-                mvn clean install package
-                """    
-        }
-        stage ("deploy")
-        {
-            
-                echo "deploy war to tomcat app server"
-                sh """
-                  scp -i /etc/key.pem -r /root/.jenkins/workspace/pipeline_pocs/first_pippeline/webapp/target/*.war ec2-user@65.0.4.77:/app/apache-tomcat-9.0.56/webapps
+                cd $WORKSPACE
                 """
-            
-        }
-        
-      }
-}
+                new checkoutSCM().call(pipelineParams)
+            }       
+           stage ('build code') 
+            {    
+                    echo "build a java code using mvn"
+                    sh """
+                    cd ${REPO}
+                    mvn clean install package
+                    """    
+            }
+            stage ("deploy")
+            {
+
+                    echo "deploy war to tomcat app server"
+                    sh """
+                      scp -i /etc/key.pem -r /root/.jenkins/workspace/pipeline_pocs/first_pippeline/webapp/target/*.war ec2-user@65.0.4.77:/app/apache-tomcat-9.0.56/webapps
+                    """
+
+            }
+
+          }
+    }
 }
