96263bc03388e9025f6f058d54f0d511f71f3a2b
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-01-28 01:04:13 +0530

    Update pushImageDR.groovy

--- src/com/poc/util/pushImageDR.groovy
+++ src/com/poc/util/pushImageDR.groovy
@@ -3,7 +3,7 @@ package com.poc.util
 def call(Map pipelineParams)
 {
   sh '''
-  docker image tag ${REPO}_image  env.REGISTRY = pipelineParams.REGISTRY:v1.0
-  docker push pipelineParams.REGISTRY:v1.0
+  docker image tag ${REPO}_image  ${REGISTRY}/${REPO}:v1.0
+  docker push ${REGISTRY}/${REPO}:v1.0
   '''
 }
