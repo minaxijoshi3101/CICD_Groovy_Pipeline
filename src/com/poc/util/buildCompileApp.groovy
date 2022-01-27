13e8f2ef102ff72021bc5fa1a8ab5d5f6b6828ee
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-01-27 17:47:39 +0530

    Update buildCompileApp.groovy

--- src/com/poc/util/buildCompileApp.groovy
+++ src/com/poc/util/buildCompileApp.groovy
@@ -5,6 +5,7 @@ def call(Map pipelineParams) {
     {
         sh '''
             cd $REPO
+            #mvn deploy copy the package to remote repo, docker registry
             mvn deploy -P docker -Ddocker.host=${DOCKER_HOST} -Ddocker.registry.name=${DOCKER_REGISTRY} -Dmaven.test.skip=true
         '''
     }
