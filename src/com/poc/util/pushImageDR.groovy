506e56d329a855e6fe1f4f46c4e7bd062f68157c
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-01-29 11:22:24 +0530

    Update pushImageDR.groovy

--- src/com/poc/util/pushImageDR.groovy
+++ src/com/poc/util/pushImageDR.groovy
@@ -3,7 +3,7 @@ package com.poc.util
 def call(Map pipelineParams)
 {
   sh '''
-
+  docker logout public.ecr.aws
   docker image tag dockerrepo ${REGISTRY}:v1.0
   docker push ${REGISTRY}:v1.0
   '''
