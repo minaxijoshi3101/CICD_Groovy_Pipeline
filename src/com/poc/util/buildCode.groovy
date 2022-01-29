075bc5159299f83909e28c57b1de3eb9c15695fd
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-01-29 19:45:23 +0530

    Update buildCode.groovy

--- src/com/poc/util/buildCode.groovy
+++ src/com/poc/util/buildCode.groovy
@@ -3,7 +3,7 @@ def call(Map pipelineParams)
 {
     sh """
     cd ${REPO}
-    export MAVEN_HOME="/root/apache-maven-3.0.5/bin"
+    export MAVEN_HOME="/root/apache-maven-3.0.5/bin/mvn"
     mvn clean install package
     docker image build -t dockerrepo .
     """    
